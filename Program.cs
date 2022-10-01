f45dab8f822d787368eb32afaf06ad5c5a1876c2
Cristian Prochnow <cristianprochnow@gmail.com>
2022-09-30 21:59:01 -0300

    feat: finish examples again

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 Account profileZeColmeia = new Account("129573892", "Zé Colmeia", 500);
 profileZeColmeia.withdraw(145, DateTime.Now, "Caldo de cana chique");
 profileZeColmeia.deposit(50, DateTime.Now, "Empréstimo do carinha que vende caldo de cana pra comprar caldo de cana");
+profileZeColmeia.deposit(50, DateTime.Now, "Coca show");
+profileZeColmeia.withdraw(500, DateTime.Now, "Pagamento da escola do Colmeinha");
 Console.WriteLine(profileZeColmeia.getData());
 Console.WriteLine();
 Console.WriteLine("Extrato");
@@ -10,8 +12,10 @@ Console.WriteLine();
 Console.WriteLine(profileZeColmeia.showExtract());
 Console.ReadKey();
 
-Account profileCatatau = new Account("129234892", "Catatau", 340);
+SpecialAccount profileCatatau = new SpecialAccount("129234892", "Catatau", 340, 400);
 profileCatatau.withdraw(230, DateTime.Now, "Caldo de cana ainda mais chique");
+profileCatatau.withdraw(100, DateTime.Now, "Um pastelzin cheio de classe");
+profileCatatau.withdraw(300, DateTime.Now, "Escola do Catatauzinho");
 Console.WriteLine(profileCatatau.getData());
 Console.WriteLine();
 Console.WriteLine("Extrato");
